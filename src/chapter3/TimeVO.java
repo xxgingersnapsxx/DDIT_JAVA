ae52d446648467d4e4177a481f2e531d53f56d92
chopperkim <chocojhkim@gmail.com>
2021-05-10 09:36:57 +0900

    move package

--- src/chapter3/TimeVO.java
+++ src/chapter3/TimeVO.java
@@ -1,4 +1,4 @@
-package homework3;
+package chapter3;
 
 public class TimeVO {
     private int hour;
--- src/chapter3/TimeVOExample.java
+++ src/chapter3/TimeVOExample.java
@@ -1,4 +1,4 @@
-package homework3;
+package chapter3;
 
 public class TimeVOExample {
     public static void main(String[] args) {
