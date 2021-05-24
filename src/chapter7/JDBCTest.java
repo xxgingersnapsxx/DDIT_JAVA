392ab801f813f5f43bc7c9c6ffe56c06200446cd
PC-03 <PC-03@DESKTOP-F1P2G7B>
2021-05-24 11:20:45 +0900

    test

--- src/chapter7/JDBCTest.java
+++ src/chapter7/JDBCTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class JDBCTest {
 	public static void main(String[] args) throws Exception {
 		LprodDao dao = new LprodDao();
+		System.out.println("test");
 		List<LprodVO> vos = dao.select();
 		System.out.println("ID\t\t구분\t\t이름");
 		for (LprodVO vo : vos) {
